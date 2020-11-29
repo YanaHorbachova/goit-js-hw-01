6f438c30264ce61bccf39e1ca093ab465c1dec05
YanaHorbachova <maya69877@gmail.com>
2020-11-29 19:23:20 +0200

    доделано задание 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,20 @@
-// let credits = 23580;
-// const pricePerDroid = 3000;
+let credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice = 0;
+let creditsNow = 0;
+let message;
 
-// const number = prompt ('Какое количество дронов желаете приобрести?')
-// const totalPrice = pricePerDroid * number;  
+let imputDroid = prompt ('Какое количество дронов желаете приобрести?')
 
-// if (number >= 0 ) {
-//     credits = credits - totalPrice;
-//     confirm.log(`Вы купили ${number} дроидов, на счету осталось ${credits} кредитов.`);
-// } else { 
-//     confirm.log('Отменено пользователем!');
-// }
+if (imputDroid !== null ) {
+    imputDroid = Number(imputDroid);
+    totalPrice = pricePerDroid * imputDroid;  
+    creditsNow = credits - totalPrice;
+    message =   totalPrice <= credits
+        ? `Вы купили ${imputDroid} дроидов, на счету осталось ${creditsNow} кредитов.`
+        : 'Недостаточно средств на счету!';
+} else { 
+    message ='Отменено пользователем!';
+}
+
+alert(message);
