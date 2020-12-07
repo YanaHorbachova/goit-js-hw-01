c5a6ff95e8b39e2fc1c8947e9ab3173725109faf
YanaHorbachova <maya69877@gmail.com>
2020-12-07 14:25:09 +0200

    Внесены правки

--- js/task-5.js
+++ js/task-5.js
@@ -1,43 +1,47 @@
-const userCountry = prompt('Выберите страну доставки');
-const normalImput = userCountry.toLowerCase();
-
 let price;
+let country;
 let massage 
+let countryName; 
 
-switch (normalImput){
-    case 'китай': {
-        price = `100 кредитов`
-        massage = `Доставка в ${userCountry} будет стоить ${price} кредитов`
-        break;
-        }
+country = prompt('Выберите страну доставки');
+if (country !== null) {
+  country = country.toLowerCase();
+  countryName = country[0].toUpperCase() + country.slice(1);
 
-    case 'чили': {
-        price = `250 кредитов`
-        massage = `Доставка в ${userCountry} будет стоить ${price} кредитов`
-        break;
-        }
+    switch (country){
+        case 'китай': {
+            price = `100 кредитов`
+            massage = `Доставка в ${countryName} будет стоить ${price} кредитов`
+            break;
+            }
 
-    case 'австралия': {
-        price = `170 кредитов`
-        massage = `Доставка в ${userCountry} будет стоить ${price} кредитов`
-        break;
-        }
+        case 'чили': {
+            price = `250 кредитов`
+            massage = `Доставка в ${countryName} будет стоить ${price} кредитов`
+            break;
+            }
 
-    case 'индия': {
-        price = `80 кредитов`
-        massage = `Доставка в ${userCountry} будет стоить ${price} кредитов`
-        break;
-        }
-    
-    case 'ямайка': {
-        price = `120 кредитов`
-        massage = `Доставка в ${userCountry} будет стоить ${price} кредитов`
-        break;
-        } 
+        case 'австралия': {
+            price = `170 кредитов`
+            massage = `Доставка в ${countryName} будет стоить ${price} кредитов`
+            break;
+            }
 
-    default: 
-        massage = 'В вашей стране доставка не доступна'
+        case 'индия': {
+            price = `80 кредитов`
+            massage = `Доставка в ${countryName} будет стоить ${price} кредитов`
+            break;
+            }
+        
+        case 'ямайка': {
+            price = `120 кредитов`
+            massage = `Доставка в ${countryName} будет стоить ${price} кредитов`
+            break;
+            } 
 
+        default: 
+            massage = 'В вашей стране доставка не доступна'
+    }
 }
 
 alert(massage);
